02b30357cdeb35c445c94c76f9120d8af78b67d0
Gronvolt <oleg.gronvolt@gmail.com>
2025-06-20 00:42:00 +0200

    Настроил подключение к БД Написал базовый Docker-compose и application.yml Подключил Kafka + KRaft, Liquibase

---
+++ src/main/java/com/neomodeon/glashatai/GlashataiApplication.java
@@ -0,0 +1,13 @@
+package com.neomodeon.glashatai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlashataiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlashataiApplication.class, args);
+    }
+
+}
